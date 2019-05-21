0e87c9723e1258712de45763678c37329bdfa0b2
gp5yk <nahuel.arjona.93@gmail.com>
2019-05-21 10:18:54 -0700

    add docstring to apple constructor

--- src/Apple.js
+++ src/Apple.js
@@ -1,5 +1,8 @@
 class Apple {
-
+  /**
+   * Spawns an apple in the board
+   * @param {{top, left}}} pos 
+   */
   constructor(pos = {top: 200, left: 200}) {
     this.node = $('<img id="apple"></img>');
     this.node.attr('src', 'src/assets/apple.jpg');
